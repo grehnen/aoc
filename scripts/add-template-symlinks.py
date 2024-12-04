33876dcaf36774d2ba1dfffe5449ad118016002e
Emil Grehn <emigr583@student.liu.se>
2024-12-04 14:03:03 +0100

    Add force option to template symlink script

--- scripts/add-template-symlinks.py
+++ scripts/add-template-symlinks.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import argparse
 
 
-def create_symlinks(target_folder_name):
+def create_symlinks(target_folder_name, force=False):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     base_dir = os.path.join(script_dir, "..")
 
@@ -27,19 +28,33 @@ def create_symlinks(target_folder_name):
 
     # Create symbolic links for each file in the target folder
     for file_name in template_files:
+        removed = False
         source_path = os.path.abspath(os.path.join(template_dir, file_name))
         link_path = os.path.abspath(os.path.join(target_folder_path, file_name))
+        if os.path.islink(link_path) and force:
+            os.remove(link_path)
+            removed = True
         try:
             os.symlink(source_path, link_path)
-            print(f"Created symlink: {link_path} -> {source_path}")
+            if removed:
+                print(f"Recreated symlink: {link_path} -> {source_path}")
+            else:
+                print(f"Created symlink: {link_path} -> {source_path}")
         except FileExistsError:
             print(f"Symlink already exists: {link_path}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python add_template_symlinks.py target_folder")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Create symlinks for template files.")
+    parser.add_argument(
+        "target_folder", help="The target folder to create symlinks in."
+    )
+    parser.add_argument(
+        "-F",
+        "--force",
+        action="store_true",
+        help="Remove and recreate existing symlinks.",
+    )
+    args = parser.parse_args()
 
-    target_folder = sys.argv[1]
-    create_symlinks(target_folder)
+    create_symlinks(args.target_folder, args.force)
