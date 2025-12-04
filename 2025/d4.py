3e466251f4a28178543aea28903ae236640c5c38
Emil Grehn <emigr583@student.liu.se>
2025-12-04 06:35:37 +0100

    Solve day 4

--- 2025/d4.py
+++ 2025/d4.py
@@ -3,5 +3,32 @@ from utils import fetch_input, Grid, Coord, Vector, ints, floats
 
 file_content: List[str] = fetch_input(__file__)
 
-for line in file_content:
-    pass
+
+def solve_1(grid: Grid) -> int:
+    count = 0
+    rolls = grid.find_all("@")
+    for roll in rolls:
+        nv = grid.neighbor_values(roll)
+        if nv.count("@") < 4:
+            count += 1
+    return count
+
+
+def solve_2(grid: Grid) -> int:
+    count = 0
+    removed = True
+    while removed:
+        removed = False
+        rolls = grid.find_all("@")
+        for roll in rolls:
+            nv = grid.neighbor_values(roll)
+            if nv.count("@") < 4:
+                count += 1
+                grid[roll] = "."
+                removed = True
+    return count
+
+
+grid = Grid(file_content)
+print(solve_1(grid))
+print(solve_2(grid))
