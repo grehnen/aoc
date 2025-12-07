fed6ace5c7f282c4438cee406b225fea099db570
Emil Grehn <emigr583@student.liu.se>
2025-12-07 06:29:06 +0100

    Solve day 7

--- 2025/d7.py
+++ 2025/d7.py
@@ -1,20 +1,67 @@
 from typing import List
 from utils import fetch_input, Grid, Coord, Vector, ints, floats
+from functools import cache
 
 file_content: List[str] = fetch_input(__file__)
 
+example = [
+    ".......S.......",
+    "...............",
+    ".......^.......",
+    "...............",
+    "......^.^......",
+    "...............",
+    ".....^.^.^.....",
+    "...............",
+    "....^.^...^....",
+    "...............",
+    "...^.^...^.^...",
+    "...............",
+    "..^...^.....^..",
+    "...............",
+    ".^.^.^.^.^...^.",
+    "...............",
+]
 
-def solve_1():
+RIGHT = Vector(1, 0)
+LEFT = Vector(-1, 0)
+DOWN = Vector(0, 1)
+
+
+def solve_1(grid: Grid, start):
     count = 0
+    to_check = {start + DOWN}
+    already_checked = set()
+    while to_check:
+        pos = to_check.pop()
+        if not grid.is_in_bounds(pos):
+            continue
+        cell = grid[pos]
+        if cell == "^":
+            count += 1
+            if pos + LEFT not in already_checked:
+                to_check.add(pos + LEFT)
+            if pos + RIGHT not in already_checked:
+                to_check.add(pos + RIGHT)
+        elif pos + DOWN not in already_checked:
+            to_check.add(pos + DOWN)
+        already_checked.update(to_check)
 
     return count
 
 
-def solve_2():
-    count = 0
+@cache
+def solve_2(grid: Grid, position: Coord) -> int:
+    if position.y >= grid.height:
+        return 1
+    if grid[position] == "^":
+        return solve_2(grid, position + LEFT) + solve_2(grid, position + RIGHT)
+    else:
+        return solve_2(grid, position + DOWN)
 
-    return count
 
+grid = Grid(file_content)
+start = grid.find("S")
 
-print(solve_1())
-print(solve_2())
+print(solve_1(grid, start))
+print(solve_2(grid, start))
