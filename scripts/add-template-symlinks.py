7fd5d920a780fff6442d5d2fef66ac4f69b86238
Emil Grehn <emigr583@student.liu.se>
2025-12-11 17:17:59 +0100

    Refactor symlink creation script

--- scripts/add-template-symlinks.py
+++ scripts/add-template-symlinks.py
@@ -1,41 +1,41 @@
-import os
+from os import path, symlink, remove, listdir, makedirs
 import sys
 import argparse
 
 
 def create_symlinks(target_folder_name, force=False):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    base_dir = os.path.join(script_dir, "..")
+    script_dir = path.dirname(path.abspath(__file__))
+    base_dir = path.join(script_dir, "..")
 
     # Ensure the target folder exists
-    target_folder_path = os.path.join(base_dir, target_folder_name)
-    if not os.path.isdir(target_folder_path):
-        print("Target folder does not exist.")
-        sys.exit(1)
+    target_folder_path = path.abspath(path.join(base_dir, target_folder_name))
+    if not path.isdir(target_folder_path):
+        makedirs(target_folder_path)
+        print(f"Created target folder: {target_folder_path}")
 
     # Get the templates
-    template_dir = os.path.join(base_dir, "template")
-    if not os.path.isdir(template_dir):
+    template_dir = path.join(base_dir, "template")
+    if not path.isdir(template_dir):
         print("Template directory does not exist.")
         sys.exit(1)
 
     template_files = [
         f
-        for f in os.listdir(template_dir)
-        if os.path.isfile(os.path.join(template_dir, f))
-        and not f in [".gitignore", "template.py"]
+        for f in listdir(template_dir)
+        if path.isfile(path.join(template_dir, f))
+        and f not in [".gitignore", "template.py"]
     ]
 
     # Create symbolic links for each file in the target folder
     for file_name in template_files:
         removed = False
-        source_path = os.path.abspath(os.path.join(template_dir, file_name))
-        link_path = os.path.abspath(os.path.join(target_folder_path, file_name))
-        if os.path.islink(link_path) and force:
-            os.remove(link_path)
+        source_path = path.abspath(path.join(template_dir, file_name))
+        link_path = path.abspath(path.join(target_folder_path, file_name))
+        if path.islink(link_path) and force:
+            remove(link_path)
             removed = True
         try:
-            os.symlink(source_path, link_path)
+            symlink(source_path, link_path)
             if removed:
                 print(f"Recreated symlink: {link_path} -> {source_path}")
             else:
