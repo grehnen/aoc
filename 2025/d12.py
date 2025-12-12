e53276d968da342ce882f288c07c58048423bbee
Emil Grehn <emigr583@student.liu.se>
2025-12-12 07:02:53 +0100

    Solve day 12🥳

--- 2025/d12.py
+++ 2025/d12.py
@@ -1,20 +1,41 @@
-from typing import List
+from typing import List, Tuple
 from utils import fetch_input, Grid, Coord2, Vector2, ints, floats, Vector3, Coord3
 
-file_content: List[str] = fetch_input(__file__)
 
-
-def solve_1():
+def solve_1(shapes: List[Grid], regions: List[Tuple[int, int, List[int]]]):
     count = 0
+    for region in regions:
+        x_size, y_size, shape_counts = region
+        all_shapes: List[Grid] = []
+        for i in range(6):
+            all_shapes.extend([shapes[i]] * shape_counts[i])
+
+        if sum([x.count("#") for x in all_shapes]) < x_size * y_size:
+            count += 1
 
     return count
 
 
 def solve_2():
-    count = 0
+    count = 568017948388515749645145398817153395
 
-    return count
+    return count.to_bytes(15).decode("utf-8")
 
 
-print(solve_1())
-print(solve_2())
+if __name__ == "__main__":
+    file_content: List[str] = fetch_input(__file__)
+
+    shapes: List[Grid] = []
+    for shape_i in range(6):
+        shape = Grid(file_content[shape_i * 5 + 1 : shape_i * 5 + 4])
+        shapes.append(shape)
+
+    regions: List[Tuple[int, int, List[int]]] = []
+    last_empty = max(i for i, line in enumerate(file_content) if line == "")
+    regions = []
+    for region in file_content[last_empty + 1 :]:
+        w, h, sc0, sc1, sc2, sc3, sc4, sc5 = ints(region)
+        regions.append((w, h, [sc0, sc1, sc2, sc3, sc4, sc5]))
+
+    print(solve_1(shapes, regions))
+    print(solve_2())
